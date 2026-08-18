c0f00116c0635f05198de036b384706ef8af0a72
radit <rdithardiansyah@gmail.com>
2026-08-18 12:31:18 +0700

    next cari cara deploy

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 (tidak)LUAng
 
-adalah aplikasi gokil mencengangkan yang saya buat untuk belajar LUA. kenapa memilih pomdoro? saya pribadi suka menggunakan metode ini untuk belajar, karena saya suka terbawa suasana saat sedang istirahat sehingga pomodoro mencegah belajar 2 menit istirahat 2 jam :DD
+adalah aplikasi gokil mencengangkan yang saya buat untuk belajar LUA. itulah mengapa namanya tidakLUAng karena LUA, bahasa pemrograman LUA hehe xixi wkwk
+
+kenapa memilih pomdoro? saya pribadi suka menggunakan metode ini untuk belajar, karena saya suka terbawa suasana saat sedang istirahat sehingga pomodoro mencegah belajar 2 menit istirahat 2 jam :DD
--- main.lua
+++ main.lua
@@ -1,17 +1,22 @@
 x = 0
 start = false;
+state = "bekerja"
+break30Set = 0;
+
+function love.load()
+     knight = love.graphics.newImage("knight.png")
+    knight:setFilter("nearest", "nearest")
+
+     knight_sleep = love.graphics.newImage("knight-sleep.png")
+    knight_sleep:setFilter("nearest", "nearest")
+end
 
 function love.update(dt)
-    local break30Set = 0;
-    local state = "bekerja"
-
-    character = love.graphics.newImage("knight.png")
-    character:setFilter("nearest", "nearest")
 
     if start == true then
         x = x + dt
     if state == "bekerja" then
-        if x >= 15 then
+        if x >= 1500 then
             x = 0
              break30Set = break30Set + 1
             state = "istirahat"
@@ -19,13 +24,14 @@ function love.update(dt)
     end
 
    if break30Set == 4 and state == "istirahat" then
-        if x >= 33 then
+        if x >= 1800 then
             x = 0;
             break30Set = 0
             state = "bekerja"
+         
         end
    elseif state == "istirahat" then
-     if x >= 18 then
+     if x >= 300 then
             x = 0
             state = "bekerja"
         end
@@ -36,16 +42,30 @@ function love.update(dt)
      
 end
 
-function love.mousepressed(button)
+function love.mousepressed(x, y, button)--temskuy parameter wajib
    if button == 1 then 
      start = true
    elseif button == 2 then
+    
     start = false
    end
 end
 
 function love.draw()
-        love.graphics.draw(character, -5, -20, 0, 4, 4)
+  local minutes = math.floor(x/60)
+  local seconds = math.floor(x%60)
+
+    if state == "bekerja" then
+        love.graphics.draw(knight_sleep, -5, -20, 0, 4, 4)
+    elseif state == "istirahat" then
+         love.graphics.draw(knight, -5, -20, 0, 4, 4)
+    end
+        
+    if start == false and state == "bekerja" then
+          love.graphics.draw(knight, -5, -20, 0, 4, 4)
+            love.graphics.print(string.format("%02d:%02d", minutes, seconds))
+        
+    end
         
 end
 
